4de387f4cc8fdb22109bd4e4cea97f7f5cd26eab
yardenMVC <yarden4869@gmail.com>
2025-03-29 15:19:19 +0300

    basic structure

---
+++ server/src/models/flight_monitor_models.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const flightDataSchema =new mongoose.Schema({ 
+  altitude: {type: Number,required: true,min: 0,max: 3000},
+  his: {type: Number,required: true,min: 0,max: 360},
+  adi: {type: Number,required: true,min: -100,max: 100}
+});
+module.exports = mongoose.model('flightData',flightDataSchema);
